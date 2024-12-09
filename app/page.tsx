7c39eb7dc42048a9310d27b122105a25068c6124
Trey Murray <treymurray00@outlook.com>
2024-12-09 08:09:51 -0400

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import Image from "next/image";
 import CameraComponent from "@/components/camera";
-import FaceDetectionComponent from "@/components/face-detection-component";
 export default function Home() {
   return (
     <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <CameraComponent></CameraComponent>
-      {/* <FaceDetectionComponent></FaceDetectionComponent> */}
     </div>
   );
 }
